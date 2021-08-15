2b68fc4965b449376abfc09b926bb20635a7e328
Andy Pearson <andy@londonparkour.com>
2021-08-15 18:28:22 +0100

    headless

--- get_title.js
+++ get_title.js
@@ -6,13 +6,13 @@
     (async () => {
 
         const browser = await puppeteer.launch({
-            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            args: ['--no-sandbox'],
             executablePath: process.env.PUPPETEER_EXECUTABLE_PATH, // set by docker container
             headless: true,
         });
         
         const page = await browser.newPage()
-        await page.goto('https://londonparkour.com/')
+        await page.goto('https://www.google.com/')
         const title = await page.title()
         console.log(title)
         await browser.close()
